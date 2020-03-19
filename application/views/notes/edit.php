e7d04f9daa6946bf4c015b11987d747efb806d54
Udeni dilrukshi <40499515+udenidilru@users.noreply.github.com>
2020-03-19 08:44:50 +0530

    add bootstrap

--- application/views/notes/edit.php
+++ application/views/notes/edit.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CRUD System</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>bootstrap/css/bootstrap.min.css">
     <style>
         .mt40{
             margin-top: 40px;
